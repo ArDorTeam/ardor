120d6c0cb4d7f786456750ad159e44ef5417723e
gzg1023 <guodev@outlook.com>
2023-08-27 21:59:51 +0800

    fix: fix router document render

--- components/container.tsx
+++ components/container.tsx
@@ -1,16 +1,15 @@
 
 "use client";
+import { Path, SlotID } from "@/app/constant"; // 路由列表
 import { About } from "@/pages/about/about";
 import { Article } from "@/pages/article/article";
 import { Home } from "@/pages/home/home";
+import { useEffect, useState } from "react";
 import {
     Route,
     HashRouter as Router,
     Routes,
 } from "react-router-dom";
-
-import { Path, SlotID } from "@/app/constant"; // 路由列表
-
 
 
 function Screen() {
@@ -19,23 +18,42 @@ function Screen() {
             {(
                 <>
                     <div id={SlotID.AppBody}>
-                        <Routes>
+                         <Routes>
                             <Route path={Path.Home} element={<Home />} />
                             <Route path={Path.Article} element={<Article />} />
                             <Route path={Path.About} element={<About />} />
-                        </Routes>
+                        </Routes> 
                     </div>
                 </>
             )}
         </div>
     );
 }
+export function Loading() {
+    return (
+      <div>
+        loading
+      </div>
+    );
+  }
 
+  
+const useHasHydrated = () => {
+    const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+    useEffect(() => {
+      setHasHydrated(true);
+    }, []);
+  
+    return hasHydrated;
+  };
 
 export function Container() {
+    if (!useHasHydrated()) {
+        return <Loading />;
+      }
     return (
         <Router>
             <Screen />
-        </Router>
+      </Router>
     );
 }
