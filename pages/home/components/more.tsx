6257ddcb28f0a07ae9fdf4d4438c35a615704759
liangyj <liangyj@systec.com.cn>
2023-09-24 19:33:38 +0800

    feat: 详情页兼容byteMD

--- pages/home/components/more.tsx
+++ pages/home/components/more.tsx
@@ -1,6 +1,7 @@
 import { ListItemType } from "@/lib/types";
 import dayjs from 'dayjs';
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 import styles from "./home.module.scss";
 
 interface ItemProps {
@@ -9,7 +10,7 @@ interface ItemProps {
 }
 
 interface listInf {
-  list:ListItemType[] | undefined
+  list: ListItemType[] | undefined
 }
 
 export function Item({ info, mini = false }: ItemProps) {
@@ -17,29 +18,32 @@ export function Item({ info, mini = false }: ItemProps) {
   const titleClassName = `${mini ? "text-2xl" : "text-4xl"} font-semibold break-words`;
   const contentClassName = `${mini ? "text-base text-gray-500" : "text-2xl"} mt-4 text-2xl`;
 
+
   return (
     <div className={itemClassName}>
-      <h2 className={titleClassName}>{info.title}</h2>
-      <div className={contentClassName}>{info.sub_title}</div>
-      <div className="flex items-center gap-1 mt-6 text-sm">
-        <span className="text-blue-600/100">{dayjs(info.gmt_modified).format('YYYY-MM-DD')}</span>
-        <span className="text-zinc-400">{info.visits} READ</span>
-      </div>
+      <Link to={`/detail/${info.article_id}`}>
+        <h2 className={titleClassName}>{info.title}</h2>
+        <div className={contentClassName}>{info.sub_title}</div>
+        <div className="flex items-center gap-1 mt-6 text-sm">
+          <span className="text-blue-600/100">{dayjs(info.gmt_modified).format('YYYY-MM-DD')}</span>
+          <span className="text-zinc-400">{info.visits} READ</span>
+        </div>
+      </Link>
     </div>
   );
 }
 
-export default function More({list}:listInf) {
+export default function More({ list }: listInf) {
   const length = 10
-  const [offset,setOffset] = useState(1)
+  const [offset, setOffset] = useState(1)
   const count = length * offset
   return (
     <div>
       <div className={styles.title}>MORE ISSUES</div>
-      {list && list.slice(0,count).map((it) => (
+      {list && list.slice(0, count).map((it) => (
         <Item key={it.id} info={it} />
       ))}
-      {list && list.length > count && <div className='mt-6 text-center cursor-pointer text-zinc-400 hover:text-gray-500' onClick={()=>setOffset(offset+1)}>点击加载更多</div>}
+      {list && list.length > count && <div className='mt-6 text-center cursor-pointer text-zinc-400 hover:text-gray-500' onClick={() => setOffset(offset + 1)}>点击加载更多</div>}
     </div>
   );
 }
